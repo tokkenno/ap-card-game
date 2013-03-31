4f191d1a68c7889ad82a01810f96f9fbe1464c60
Aitor González Fernández <reimashi@gmail.com>
2013-03-31 16:22:12 +0200

    Primera implementación

---
+++ main.c
@@ -0,0 +1,43 @@
+/* 
+ * File:        main.c
+ * Description: Punto de entrada del programa. Contiene los bucles del juego.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <cv.h>
+#include <highgui.h>
+
+#include "cards.h"
+
+/*
+ * Variables necesarias para el bucle principal del juego
+ */
+
+// Puntuacion del juego actual
+int points = 0; 
+
+/*
+ * Bucle principal del juego
+ */
+void loopPpal()
+{
+    
+}
+
+/*
+ * Punto de entrada del programa.
+ */
+int main(int argc, char** argv) {
+    
+    loadImage("ghoald");
+    
+    cvWaitKey(0);
+    
+    // Destruimos todas las ventanas
+    cvDestroyAllWindows();
+
+    return (EXIT_SUCCESS);
+}
+
