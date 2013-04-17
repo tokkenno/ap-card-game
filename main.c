22eff1ab38e0d4770e8d5f5ff236a72f75baf664
riopedre2001 <riopedre2001@gmail.com>
2013-04-17 21:59:44 +0200

    Juego principal => Iniciada la ventana de juego.

--- main.c
+++ main.c
@@ -10,22 +10,43 @@
 
 // Nombre de la ventana del juego
 static char* window_name = "ap-card-game (alpha)";
-
+//Tamaño lateral de las cartas
+static int dim = 256;
 // Puntuacion del juego actual
 int points = 0;
-
+//Primera carta del deck
+static char* cards1 = "/decks/animals/01.png";
 // Punto de entrada del programa.
+
 int main(int argc, char** argv) {
     int quitcondition = 0;
     SGE_Window wppal;
-    wppal = SGE_Init(window_name, 600, 400);
-    
+    wppal = SGE_Init(window_name, 800, 600);
+
     // Bucle principal del juego
-    while(!quitcondition)
-    {
-        SGE_Update(&wppal);
-    }
     
+        SGE_Surface* bkg;
+        *bkg = SGE_CreateSurface(800, 600);
+        SGE_Surface* card1;
+        *card1=SGE_LoadImage(cards1);
+        
+        int x, y;
+        SGE_Rectangle pos;
+        pos.width = dim;
+        pos.height = dim;
+        
+        for (x = 0; x < 800; x += 400)  
+            for (y = 0; y < 600; y += 300) {
+                pos.pos_x = x;
+                pos.pos_y = y;
+                SGE_PasteSurface(bkg, card1, pos);
+            }
+        
+        
+        SGE_Update(&wppal);
+        cvWaitKey(5000);
+    
+
     SGE_Quit(&wppal);
 
     return 0;
