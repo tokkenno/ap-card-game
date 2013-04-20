37c95e4563c20ec9fa797d3c54a5979c6d33b9b0
Aitor González Fernández <reimashi@gmail.com>
2013-04-20 21:38:46 +0200

    Juego principal => Dibujando superficies de prueba

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 
 #include "sge/sge.h"
+#include <cv.h>
+#include <highgui.h>
 
 // Nombre de la ventana del juego
 static char* window_name = "ap-card-game (alpha)";
@@ -15,39 +17,29 @@ static int dim = 256;
 // Puntuacion del juego actual
 int points = 0;
 //Primera carta del deck
-static char* cards1 = "/decks/animals/01.png";
+static char* cards1 = "/home/aitor/Desarrollo/APCardGame/dist/Debug/GNU-Linux-x86/decks/animals/01.png";
 // Punto de entrada del programa.
 
 int main(int argc, char** argv) {
     int quitcondition = 0;
-    SGE_Window wppal;
+    SGE_Window* wppal;
     wppal = SGE_Init(window_name, 800, 600);
 
     // Bucle principal del juego
     
-        SGE_Surface* bkg;
-        *bkg = SGE_CreateSurface(800, 600);
-        SGE_Surface* card1;
-        *card1=SGE_LoadImage(cards1);
-        
-        int x, y;
-        SGE_Rectangle pos;
-        pos.width = dim;
-        pos.height = dim;
-        
-        for (x = 0; x < 800; x += 400)  
-            for (y = 0; y < 600; y += 300) {
-                pos.pos_x = x;
-                pos.pos_y = y;
-                SGE_PasteSurface(bkg, card1, pos);
-            }
-        
-        
-        SGE_Update(&wppal);
-        cvWaitKey(5000);
+    SGE_Surface card1 = SGE_LoadImage(cards1);
     
+    SGE_Rectangle pos = SGE_CreateRectangle(dim, dim, 0, 0);
 
-    SGE_Quit(&wppal);
+    // Pega una carta en el buffer de la pantalla
+    SGE_PasteSurface(&wppal->imgBuffer, &card1, pos);
+    
+    // Muestra la imagen del buffer en la pantalla y lo vacia
+    SGE_Update();
+    
+    SGE_Delay(5000);
+    
+    SGE_Quit();
 
     return 0;
 }
