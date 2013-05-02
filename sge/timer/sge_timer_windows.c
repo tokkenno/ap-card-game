5061e68d6f4e333226ae9afca911f262324d5296
Aitor González Fernández <reimashi@gmail.com>
2013-05-02 07:04:57 +0200

    SGE_Delay() ahora funciona tambien en Windows

--- sge/timer/sge_timer_windows.c
+++ sge/timer/sge_timer_windows.c
@@ -1,6 +1,7 @@
 #ifdef _WIN32
 
 #include "sge_timer_windows.h"
+#include <windows.h>
 
 unsigned long SGE_GetTicksWindows ()
 {
@@ -9,7 +10,7 @@ unsigned long SGE_GetTicksWindows ()
 
 void SGE_DelayWindows (unsigned long ms)
 {
-    
+    Sleep(ms);
 }
 
 #endif
