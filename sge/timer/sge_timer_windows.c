230b6991abf9b7bc0e93bec2a19d0a8f83502bee
Aitor González Fernández <reimashi@gmail.com>
2013-05-02 07:16:04 +0200

    SGE_GetTicks() ahora funciona tambien en Windows

--- sge/timer/sge_timer_windows.c
+++ sge/timer/sge_timer_windows.c
@@ -2,10 +2,18 @@
 
 #include "sge_timer_windows.h"
 #include <windows.h>
+#include <winbase.h>
+
+static unsigned long _w_ticks;
 
 unsigned long SGE_GetTicksWindows ()
 {
+    if (!_w_ticks)
+        _w_ticks = GetTickCount();
     
+    long ticks = _w_ticks;
+    _w_ticks = GetTickCount();
+    return _w_ticks - ticks;
 }
 
 void SGE_DelayWindows (unsigned long ms)
